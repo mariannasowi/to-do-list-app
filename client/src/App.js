853358141549d72227fa8bec4deb103150aa9eb3
Marianna Sowinska <marianna.sowi@gmail.com>
2020-12-21 19:00:52 +0100

    Module 28.4 bug web site

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,81 @@
 import React from 'react';
+import io from 'socket.io-client';
+import uuid from 'uuid';
 
 class App extends React.Component {
+  state = {
+    tasks: [],
+    taskName: '',
+  }
+
+  componentDidMount() {
+    this.socket = io('localhost:8000');
+    this.socket.emit('updateData');
+    this.socket.on('addTask', task => this.addTask(task));
+    this.socket.on('removeTask', (taskIndex) => this.removeTask(taskIndex));
+    this.socket.on('updateData', tasksServer => this.setState({ tasks: tasksServer }));
+    this.socket.on('editRecord', (taskChange) => this.editRecord(taskChange.taskChange, taskChange.id))
+  }
+
+  removeTask(taskIndex, event) {
+    if(event !== undefined) {
+      this.socket.emit('removeTask', taskIndex);
+    }
+    this.setState({tasks: this.state.tasks.filter(task => task.id !== taskIndex)});
+  }
+
+  updateTaskName(value) {
+    this.setState({ taskName: value });
+  }
+
+  addTask(task) {
+    this.setState({ tasks: [...this.state.tasks, {id: task.id, task: task.task}] });
+  }
+
+  editRecord(taskChange, id, event) {
+    if(event !== undefined) {
+      this.socket.emit('editRecord', ({taskChange, id}));
+    }
+    this.setState({tasks: this.state.tasks.map(task => (
+      task.id === id ? {id: id, task: taskChange} : {id: task.id, task: task.task}
+    ))});
+  }
+
+  submitForm(event) {
+    event.preventDefault();
+    const id = uuid();
+    this.socket.emit('addTask', {id: id, task: this.state.taskName});
+    this.addTask({id: id, task: this.state.taskName});
+    this.updateTaskName('');
+  }
 
   render() {
     return (
       <div className="App">
-    
-        <header>
-          <h1>ToDoList.app</h1>
-        </header>
-    
-        <section className="tasks-section" id="tasks-section">
-          <h2>Tasks</h2>
-    
-          <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
-          </ul>
-    
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-            <button className="btn" type="submit">Add</button>
-          </form>
-    
-        </section>
-      </div>
+
+      <header>
+        <h1>ToDoList.app</h1>
+      </header>
+  
+      <section className="tasks-section" id="tasks-section">
+        <h2>Tasks</h2>
+  
+        <ul className="tasks-section__list" id="tasks-list">
+          {this.state.tasks.map(task => (
+            <li className="task" key={task.id}>
+              <input className="text-input text-task-input" type="text" value={task.task} onChange={event => this.editRecord(event.currentTarget.value, task.id, event)}></input>
+              <button className="btn btn--red" onClick={event => this.removeTask(task.id,event) }>Remove</button>
+            </li>
+          ))}
+        </ul>
+  
+        <form id="add-task-form" onSubmit={event => this.submitForm(event)}>
+          <input className="text-input" value={this.state.taskName} onChange={event => this.updateTaskName(event.currentTarget.value)} autoComplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <button className="btn" type="submit">Add</button>
+        </form>
+  
+      </section>
+    </div>
     );
   };
 
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
-});
+
+const tasks = [];
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+    console.log('Not found...');
+});
+
+const server = app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    socket.on('updateData', () => {
+        socket.emit('updateData', tasks);
+    });
+
+    socket.on('addTask', (task) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+    });
+
+    socket.on('removeTask', (taskIndex) => {
+        tasks.splice(tasks.indexOf(tasks.filter(task => task.id === taskIndex)[0]), 1);
+        socket.broadcast.emit('removeTask', taskIndex);
+    });
+
+    socket.on('editRecord', (taskChange) => {
+        tasks.map(task => {if(task.id === taskChange.id) task.task = taskChange.taskChange});
+        socket.broadcast.emit('editRecord', (taskChange));
+    });
 });
